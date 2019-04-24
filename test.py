4a607705b55485e705162650838daa09c06308ed
spelavasle <49947097+spelavasle@users.noreply.github.com>
2019-04-24 15:08:27 +0200

    Trying to get a merge conflict

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 # Napiši program, ki izpiše prvih 200 praštevil
 
-def je-prastevilo(n):
-    for i in range(2, n):
+def je-prastlikujzhtgrfedevilo(n):
+    for i in range(2, n):tvgbfsbfd
         if n % i == 0:
             return False
     return True
 
 for x in range(2, 201):
     if je_prastevilo(x):
-        print(x)
+        print(x)wsergh
+        
+        hsedfh
+        wsrthhth 4
